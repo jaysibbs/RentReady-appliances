d60a33445b6cd0937d74b1267ed0013cd8688389
Jabulani <sibbslani@rentreadyappliances.org>
2026-05-25 09:46:32 +0100

    Fix night white theme overrides

--- theme-init.js
+++ theme-init.js
@@ -1,11 +1,23 @@
 (function () {
+  var override = "";
+
+  try {
+    override = new URLSearchParams(window.location.search).get("theme") || "";
+  } catch (error) {
+    override = "";
+  }
+
+  if (override !== "day" && override !== "night") {
+    override = "";
+  }
+
   var hour = Number(new Intl.DateTimeFormat("en-GB", {
     hour: "numeric",
     hour12: false,
     timeZone: "Europe/London",
   }).format(new Date()));
 
-  var theme = hour >= 7 && hour < 19 ? "day" : "night";
+  var theme = override || (hour >= 7 && hour < 19 ? "day" : "night");
   document.documentElement.setAttribute("data-theme", theme);
   document.documentElement.style.colorScheme = theme === "night" ? "light" : "dark";
 }());
